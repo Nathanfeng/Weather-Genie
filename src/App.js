09c856f50b6b282be75caddb1f6c2c26e09c6bac
Nathan Feng <nathanlfeng@gmail.com>
2018-10-17 19:24:16 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Weather App</h1>
+        <h1>Weather Genie</h1>
         <Form/>
       </div>
     );
